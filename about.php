7e4eb49a4074c87fcc9249de0c8594876834ed2d
kerkss <gsmabarrai@gmail.com>
2022-04-27 16:09:07 +0800

    burat
    haha

---
+++ about.php
@@ -0,0 +1,17 @@
+<h1><?php echo $title;?></h1>
+<p>About BCP
+At Bestlink College of the Philippines, We provide and promote quality education with modern and unique techniques to able to enhance the skill and the knowledge of our dear students to make them globally competitive and productive citizens.
+
+<h3>MV Campus</h3>
+<li>Millionaire’s Village Campus
+Lot 762 cor. Topaz St. and Sapphire St., Millionaire’s Village, Novaliches Quezon City, Philippines
+Contact #: 463-8787, 799-6617</li>
+
+<h3>Main Campus</h3>
+<li>#1071 Brgy. Kaligayahan, Quirino Highway Novaliches Quezon City, Philippines 1123
+Contact #: 417-4355</li>
+
+<h3>Bulacan Campus</h3>
+<li>IPO Road, Barangay Minuyan Proper City of San Jose Del Monte, Bulacan
+Contact #: (044)797-2949</li>
+
---
+++ home.php
@@ -0,0 +1,33 @@
+<!-- <h1><?php echo $title;?></h1> -->
+
+<div class="col-lg-12">  
+
+    <h3 style="text-align:center;"> Bestlink College of the Philippines</h3> 
+    <h4> At Bestlink College of the Philippines, We provide and promote quality education with modern and unique techniques to able to enhance the skill and the knowledge of our dear students to make them globally competitive and productive citizens.<h4>
+</div>
+<div  class="col-lg-6">
+  <h4>Vision</h4> 
+  <p>Center of Academic Excellence Delivering Quality Service to all.</p>
+  <h4>Mission</h4>
+  <p> Bestlink College of the Philippines provide and promote quality education with modern and unique techniques to able to enhance the skill and the knowledge of our dear students to make them globally competitive and productive citizens.</p>
+  <h4>Goals</h4> 
+  <ol>
+    <li>Globally competitive graduates</li>  
+    <li>Institutionalize research culture</li> 
+    <li>Responsive and sustainable extension services</li> 
+    <li>Maximized profit of variable agro-industrial business ventures</li>  
+    <li>Effective and efficient administration </li>  
+  </ol>
+
+  <h4>Objectives</h4> 
+   <ol>
+      <li>Create an environment of shared leadership and responsibilities with competent administrator.</li>
+      <li>Provide relevant trainings and seminar to faculty, staff, and student.</li>
+      <li>Produce highly competitive graduates.</li>
+      <li>Conduct relevant and updated researches</li>
+      <li>Extend financial support and manpower for outreach activities.</li> 
+   </ol> 
+</div>
+<div  class="col-lg-6">
+  <p>Bestlink College of the Philippines has a mission and vision that the BCP students must be know. “To produce self-motivated and self-directed individual who aims for academic excellence, God-fearing, peaceful, wealthy, productive and successful citizens” is the Bestlink College of the Philippines’s mission. Its purpose is to give students the opportunity to be competitive enough and to boost their confidence. BCP guide students to success and to be active participants in our society. BCP trains students to be self-motivated and self-directed individuals that may benefit the successfulness and activeness way to their future. And also to give realizations to the BCP students to be aware to their strength and weaknesses that can be improve. ”Bestlink College of the Philippines is committed to provide and promote quality education, with a unique, modern and researched-based curriculum with delivery systems geared towards excellence” is the Bestlink College of the Philippines’s vision. Its purpose is to give students a good education and a research-based knowledge that they can use in the future. It is how the Bestlink College of the Philippines wants to happen to their students, an excellence education and unique way on how they train students that gives a remarkable experience that the students will never forget.
+Bestlink College of the Philippines’s mission and vision is committed in providing quality education to our youth. BCP meets and provides adequate facilities and services not to mention the competent and qualified instructors eagerly and enthusiastically extending and sharing their help for the welfare of the studentry.BCP vision aspires students to be a synergy of diverse and highly qualified students interacting with dedicated scholars, teachers and practitioners in a dynamic and student-centered environment. BCP mission offers students a vibrant and dynamic environment from which to enter the legal professional. They encourage students to be lifelong professionals who are actively engaged in the development of our society. Also they provide a strong foundation to fulfil the dreams of their students. Bestlink College of the Philippines give students the opportunities to concentrate on specialty areas in order to come up their skills, hands-on experienced in the classroom and through public service.</p>
